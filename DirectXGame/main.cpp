821f59fa7be24084cd293a845c82b73eadab86e1
osinoko <k023g0114@g.neec.ac.jp>
2025-07-28 15:40:26 +0900

    無効フラグ

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -8,7 +8,7 @@ using namespace KamataEngine;
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	// エンジンの初期化
-	KamataEngine::Initialize(L"LE3C_11_サクライ_ショウセイ_CG4");
+	KamataEngine::Initialize(L"LE3C_11_サクライ_ショウセイ_TL1");
 
 	// DirectXCommonインスタンスの取得
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
